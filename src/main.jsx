557db47e48f282de176c69e1d175e5f179c66a66
qasemB <mahdicmptr@gmail.com>
2025-02-27 17:39:56 +0330

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <div
+      className=""
+      style={{
+        backgroundColor: "black",
+        width: "120px",
+      }}
+    >
+      <span>
+        {/* comment */}
+        hello world
+      </span>
+    </div>
+    <div></div>
+    <div></div>
+    <span></span>
+  </div>
+);
