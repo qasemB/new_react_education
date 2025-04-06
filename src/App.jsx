acc352c749a3110aeffc2b0a90277a90119d089c
qasemB <mahdicmptr@gmail.com>
2025-04-06 16:16:25 +0330

    part-C01-component

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+const App = () => {
+  return (
+    <div>
+      <div
+        className=""
+        style={{
+          backgroundColor: "black",
+          width: "120px",
+        }}
+      >
+        <span>
+          {/* comment */}
+          hello world
+        </span>
+      </div>
+      <div></div>
+      <div></div>
+      <span></span>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,4 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <div>
-    <div
-      className=""
-      style={{
-        backgroundColor: "black",
-        width: "120px",
-      }}
-    >
-      <span>
-        {/* comment */}
-        hello world
-      </span>
-    </div>
-    <div></div>
-    <div></div>
-    <span></span>
-  </div>
-);
+createRoot(document.getElementById("root")).render(<App/>);
