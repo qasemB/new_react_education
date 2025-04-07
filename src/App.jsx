43743bd328779d45dbc005876d1369235b90f5ec
qasemB <mahdicmptr@gmail.com>
2025-04-07 16:52:02 +0330

    part-C3-cursor-Fragment_component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+// import { Fragment } from "react";
 import ClassComponent from "./ClassComponent";
 import FunctionalComponent from "./FunctionalComponent";
 
 const App = () => {
   return (
-    <div>
+    <>
       <FunctionalComponent />
       <ClassComponent />
-    </div>
+    </>
   );
 };
 
