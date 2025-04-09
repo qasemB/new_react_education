2f2192273e0b06db2a31c213fede27e509d6ea92
qasemB <mahdicmptr@gmail.com>
2025-04-09 19:10:26 +0330

    part-C5-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-// import { Fragment } from "react";
-import ClassComponent from "./ClassComponent";
-import FunctionalComponent from "./FunctionalComponent";
+import Car from "./Car";
 
 const App = () => {
   return (
-    <>
-      <FunctionalComponent />
-      <ClassComponent />
-    </>
+    <div>
+      <Car make="Toyota" model="Camry" year={2022} color="Silver" price="$25,000" />
+      <Car make="Honda" model="Civic" year={2023} color="Blue" price="$23,000" />
+    </div>
   );
 };
 
