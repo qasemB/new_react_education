709e29ed861da352095cd4a7260626ee2098f4ad
qasemB <mahdicmptr@gmail.com>
2025-04-17 18:33:22 +0330

    part-C13-functional_state_update

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,22 @@ const initialUsers = [
 const App = () => {
   const [users, setUsers] = useState(initialUsers);
 
-  const handleDelete = (name) => {
-    const newUsers = users.filter((user) => user.name !== name);
+  const handleDelete = (name) => {    
+    setUsers((prevUsers)=>{
+      const newUsers = prevUsers.filter((user) => user.name !== name);
+      return newUsers;
+    });
+  }
+  
+  const setSearch = (char)=>{
+    const newUsers = initialUsers.filter((user)=>user.name.toLocaleLowerCase().includes(char.toLocaleLowerCase()));
     setUsers(newUsers);
   }
 
 
   return (
     <div>
-      {/* <input type="text" onChange={(e)=>setSearch(e.target.value)} /> */}
+      <input type="text" onChange={(e)=>setSearch(e.target.value)} />
       {users.map((user) => (
         <UserInfo key={user.name} {...user} handleDelete={()=>handleDelete(user.name)} />
       ))}
