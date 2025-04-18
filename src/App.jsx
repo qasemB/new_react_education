18165d19fa243c9d6a48816b4089d305ae9f32a9
qasemB <mahdicmptr@gmail.com>
2025-04-18 20:51:08 +0330

    part-C14-what_is_Lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
 import { useState } from "react";
-import UserInfo from "./UserInfo";
-
-const initialUsers = [
-  { name: "John Doe", age: 25, email: "john.doe@example.com" },
-  { name: "Jane Doe", age: 26, email: "jane.doe@example.com" },
-  { name: "John Smith", age: 30, email: "john.smith@example.com" },
-  { name: "Jane Smith", age: 31, email: "jane.smith@example.com" },
-];
-
+import UserComponent from "./UserComponent";
+import GuestComponent from "./GuestComponent";
 
 const App = () => {
-  const [users, setUsers] = useState(initialUsers);
-
-  const handleDelete = (name) => {    
-    setUsers((prevUsers)=>{
-      const newUsers = prevUsers.filter((user) => user.name !== name);
-      return newUsers;
-    });
-  }
-  
-  const setSearch = (char)=>{
-    const newUsers = initialUsers.filter((user)=>user.name.toLocaleLowerCase().includes(char.toLocaleLowerCase()));
-    setUsers(newUsers);
-  }
-
-
+  const [isLogined, setIsLogined] = useState(false);
   return (
     <div>
-      <input type="text" onChange={(e)=>setSearch(e.target.value)} />
-      {users.map((user) => (
-        <UserInfo key={user.name} {...user} handleDelete={()=>handleDelete(user.name)} />
-      ))}
+      <button onClick={() => setIsLogined(!isLogined)}>
+        {isLogined ? "Logout" : "Login"}
+      </button>
+      
+      {isLogined ? <UserComponent /> : <GuestComponent />}
     </div>
   );
 };
---
+++ src/UserComponent.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const UserComponent = () => {
+  const [age, setAge] = useState(20);
+  return (
+    <div>
+      <h1>UserComponent</h1>
+      <button onClick={() => setAge((prev) => setAge(prev + 1))}>
+        Change Age
+      </button>
+      <ul>
+        <li>Name: Qasem</li>
+        <li>Age: {age}</li>
+        <li>Email: Qasem@gmail.com</li>
+      </ul>
+    </div>
+  );
+};
+
+export default UserComponent;
