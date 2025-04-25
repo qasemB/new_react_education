607ad8977fd77b3a83a011243a8208b8f97a7f44
qasemB <mahdicmptr@gmail.com>
2025-04-26 00:27:27 +0330

    part-D03-CSS_Module

--- src/userComponent/UserComponent.jsx
+++ src/userComponent/UserComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./UserComponent.css";
+import styles from "./UserComponent.module.css";
 
 const UserComponent = () => {
   const [age, setAge] = useState(20);
@@ -27,14 +27,14 @@ const UserComponent = () => {
   }, [age]);
 
   return (
-    <div className="user-container">
-      <h1 className="user-title">UserComponent</h1>
-      <button className="user-btn" onClick={() => setAge((prev) => prev + 1)}>
+    <div className={`some-class ${styles.userContainer}`}>
+      <h1 className={styles["user-title"]}>UserComponent</h1>
+      <button className={styles["user-btn"]} onClick={() => setAge((prev) => prev + 1)}>
         Change Age
       </button>
-      <ul className="user-info">
+      <ul className={styles["user-info"]}>
         <li>Name: Qasem</li>
-        <li>Age: <span className="user-age">{age}</span></li>
+        <li>Age: <span className={styles["user-age"]}>{age}</span></li>
         <li>Email: Qasem@gmail.com</li>
       </ul>
     </div>
