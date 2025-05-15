3d44648797bb2295fb2cce8af2c30f7943919699
qasemB <mahdicmptr@gmail.com>
2025-05-15 16:24:43 +0330

    part-G02-react_portal

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import UsersList from "./UsersList";
 
 const App = () => {
     return (
-        <div className="bg-gray-100 p-4 rounded-lg shadow">
+        <div className="bg-gray-100 p-4 rounded-lg shadow pb-20">
             <h1 className="text-2xl font-bold text-gray-800">سامانه مدیریت کاربران</h1>
             <UsersList />
+            <div className="fixed bottom-0 left-0 right-0  p-4 shadow-lg bg-blue-300">
+                <h1>فوتر</h1>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus non mollitia unde ipsa minus ipsum reprehenderit, iusto temporibus accusantium numquam officiis amet facere neque rerum, molestiae dolorum pariatur sed corrupti!
+            </div>
         </div>
     );
 };
--- src/UsersList.jsx
+++ src/UsersList.jsx
@@ -1,12 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserItem from "./UserItem";
 import { UserContext } from "./UserContext";
+import UserAdd from "./UserAdd";
 
 const UsersList = () => {
     const {users} = useContext(UserContext)
+    const [showModal, setShowModal] = useState(false)
     return (
         <div className="bg-white shadow overflow-hidden sm:rounded-md">
-            <h1 className="px-4 py-5 sm:px-6 text-lg leading-6 font-medium text-gray-900">لیست کاربران</h1>
+            <h1 className="px-4 py-2 sm:px-6 text-lg leading-6 font-medium text-gray-900">لیست کاربران</h1>
+            <div className="flex justify-end p-5">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => setShowModal(true)}>
+                    ایجاد کاربر
+                </button>
+                {showModal && <UserAdd setShowModal={setShowModal}/>}
+            </div>
             <ul className="border-t border-gray-200 divide-y divide-gray-200 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {users.map((user) => (
                     <li key={user.id} className="px-4 py-4 sm:px-6">
